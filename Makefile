15ac83d18c7f25f14eb8b17975c3144934820d2b
Adrian Perez <aperez@igalia.com>
2010-09-18 23:15:59 +0200

    Implement support for the system tray and setting window properties
    This (as trayer/fbpanel) uses EggTrayManager to implement a bare-bones
    system tray. Also, added code to properly "panelize" the window: set sticky,
    dock window type, skip pager, skip taskbar... etc.

--- Makefile
+++ Makefile
@@ -3,16 +3,21 @@
 # Adrian Perez, 2010-09-18 20:56
 #
 
-PKG_CFLAGS := $(shell pkg-config gtk+-2.0 globalmenu-server --cflags)
-PKG_LDLIBS := $(shell pkg-config gtk+-2.0 globalmenu-server --libs)
+SRCS := $(wildcard *.c)
+OBJS := $(patsubst %.c,%.o,$(SRCS))
+
+PKG_CFLAGS := $(shell pkg-config glib-2.0 gtk+-2.0 globalmenu-server --cflags)
+PKG_LDLIBS := $(shell pkg-config glib-2.0 gtk+-2.0 globalmenu-server --libs)
 
 LDFLAGS   += $(subst -lserver,-lglobalmenu-server,$(PKG_LDLIBS))
 CFLAGS    += $(PKG_CFLAGS)
 
 all: ggtraybar
 
+ggtraybar: $(OBJS)
+
 clean:
-	$(RM) ggtraybar.o ggtraybar
+	$(RM) ggtraybar $(OBJS)
 
 # vim:ft=make
 #
--- ggtraybar.c
+++ ggtraybar.c
@@ -5,9 +5,189 @@
  * Distributed under terms of the MIT license.
  */
 
+#include "eggtraymanager.h"
 #include <gnome-globalmenu/globalmenu-server.h>
 #include <gtk/gtk.h>
 #include <stdlib.h>
+#include <X11/X.h>
+#include <X11/Xatom.h>
+
+
+#ifndef GGTRAYBAR_HEIGHT
+#define GGTRAYBAR_HEIGHT 24
+#endif /* !GGTRAYBAR_HEIGHT */
+
+
+static Atom a_NET_WM_STRUT_PARTIAL = 0;
+static Atom a_NET_WM_STRUT         = 0;
+
+
+static void
+intern_x_atoms (void)
+{
+    a_NET_WM_STRUT_PARTIAL = XInternAtom (GDK_DISPLAY (), "_NET_WM_STRUT_PARTIAL", False);
+    a_NET_WM_STRUT         = XInternAtom (GDK_DISPLAY (), "_NET_WM_STRUT",         False);
+}
+
+
+static void
+configure_window (GtkWindow *w)
+{
+    g_assert (w);
+
+    gtk_window_stick                 (w);
+    gtk_window_set_title             (w, "Menu");
+    gtk_window_set_role              (w, "traybar");
+    gtk_window_set_icon_name         (w, "gtk-preferences");
+    gtk_window_set_wmclass           (w, "GGTrayBar", "ggtraybar");
+    gtk_window_set_type_hint         (w, GDK_WINDOW_TYPE_HINT_DOCK);
+    gtk_window_set_resizable         (w, FALSE);
+    gtk_window_set_decorated         (w, FALSE);
+    gtk_window_set_focus_on_map      (w, FALSE);
+    gtk_window_set_keep_above        (w, TRUE);
+    gtk_window_set_skip_pager_hint   (w, TRUE);
+    gtk_window_set_skip_taskbar_hint (w, TRUE);
+    gtk_container_set_border_width   (GTK_CONTAINER (w), 0);
+}
+
+
+static void
+set_xwin_properties (GtkWindow *w)
+{
+#if 0
+    Window xwin = GDK_WINDOW_XWINDOW (gtk_widget_get_window (GTK_WIDGET (w)));
+    gulong data[12] = { 0 };
+
+    data[2]       = gdk_screen_width ();
+    data[4 + 2*2] = 0;
+    data[5 + 2*2] = 0 + GGTRAYBAR_HEIGHT;
+
+    XChangeProperty (GDK_DISPLAY (),
+                     xwin,
+                     a_NET_WM_STRUT_PARTIAL,
+                     XA_CARDINAL,
+                     32,
+                     PropModeReplace,
+                     (unsigned char*) data,
+                     12);
+
+    XChangeProperty (GDK_DISPLAY (),
+                     xwin,
+                     a_NET_WM_STRUT,
+                     XA_CARDINAL,
+                     32,
+                     PropModeReplace,
+                     (unsigned char*) data,
+                     4);
+
+    XMoveWindow (GDK_DISPLAY (), xwin, 0, 0);
+    XSync (GDK_DISPLAY (), False);
+#endif
+
+    gtk_window_move (w, 0, 0);
+}
+
+
+
+static void
+tray_changed (GtkWidget *widget)
+{
+    gtk_widget_set_size_request (widget,
+                                 widget->allocation.width,
+                                 widget->allocation.height);
+
+    gtk_widget_hide (widget);
+
+    while (gtk_events_pending ())
+        gtk_main_iteration ();
+
+    gtk_widget_show (widget);
+    gtk_widget_set_size_request (widget, -1, -1);
+}
+
+
+static void
+tray_added (EggTrayManager *traymgr, GtkWidget *icon, void *priv)
+{
+    GtkBox *box = GTK_BOX (priv);
+
+    g_assert (traymgr);
+    g_assert (icon);
+    g_assert (priv);
+
+    gtk_box_pack_end (box, icon, FALSE, FALSE, 0);
+    gtk_widget_show (icon);
+    gdk_display_sync (gtk_widget_get_display (icon));
+
+    tray_changed (GTK_WIDGET (box));
+}
+
+
+static void
+tray_removed (EggTrayManager *traymgr, GtkWidget *icon, void *priv)
+{
+    GtkBox *box = GTK_BOX (priv);
+
+    g_assert (traymgr);
+    g_assert (icon);
+    g_assert (priv);
+
+    tray_changed (GTK_WIDGET (box));
+}
+
+
+static void
+message_sent (EggTrayManager *traymgr,
+              GtkWidget      *icon,
+              const char     *text,
+              glong           mid,
+              glong           timeout,
+              void           *data)
+{
+}
+
+
+static void
+message_cancel (EggTrayManager *traymgr,
+                GtkWidget      *icon,
+                glong           mid,
+                void           *data)
+{
+}
+
+
+static GtkWidget*
+make_traybar_widget (EggTrayManager *traymgr)
+{
+    GtkWidget *traybox;
+
+    g_assert (traymgr);
+
+    traybox = gtk_hbox_new (TRUE, 2);
+
+    g_signal_connect (traymgr,
+                      "tray_icon_added",
+                      G_CALLBACK (tray_added),
+                      traybox);
+
+    g_signal_connect (traymgr,
+                      "tray_icon_removed",
+                      G_CALLBACK (tray_removed),
+                      traybox);
+
+    g_signal_connect (traymgr,
+                      "message_sent",
+                      G_CALLBACK (message_sent),
+                      NULL);
+
+    g_signal_connect (traymgr,
+                      "message_cancelled",
+                      G_CALLBACK (message_cancel),
+                      NULL);
+
+    return traybox;
+}
+
 
 
 int
@@ -15,19 +195,52 @@ main (int argc, char **argv)
 {
     GnomenuGlobalMenuBar *menubar;
     GtkWidget *window;
+    GtkWidget *hbox;
+    GdkScreen *screen;
 
     g_type_init ();
     gtk_init (&argc, &argv);
 
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title (GTK_WINDOW (window), "Menubar");
+    screen = gtk_widget_get_screen (window);
+
+    configure_window (GTK_WINDOW (window));
 
     menubar = gnomenu_global_menu_bar_new ();
 
+    hbox = gtk_hbox_new (FALSE, 5);
+
+    gtk_box_pack_start (GTK_BOX (hbox),
+                        GTK_WIDGET (menubar),
+                        FALSE,
+                        FALSE,
+                        0);
+
+    if (egg_tray_manager_check_running (screen)) {
+        g_warning ("Another tray manager already running, tray disabled.\n");
+    }
+    else {
+        EggTrayManager *traymgr = egg_tray_manager_new ();
+        if (!egg_tray_manager_manage_screen (traymgr, screen))
+            g_printerr ("Tray manager could not manage screen");
+        gtk_box_pack_end (GTK_BOX (hbox),
+                          make_traybar_widget (traymgr),
+                          FALSE,
+                          FALSE,
+                          0);
+    }
+
     gtk_container_add (GTK_CONTAINER (window),
-                       GTK_WIDGET (menubar));
+                       GTK_WIDGET (hbox));
+
+    gtk_widget_set_size_request (window,
+                                 gdk_screen_width (),
+                                 GGTRAYBAR_HEIGHT);
 
     gtk_widget_show_all (window);
+
+    intern_x_atoms ();
+    set_xwin_properties (GTK_WINDOW (window));
 
     gtk_main ();
 
